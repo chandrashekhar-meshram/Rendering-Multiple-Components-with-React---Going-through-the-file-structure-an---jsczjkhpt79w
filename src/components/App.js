ea32313895ec21dca2fb6f260ce5661312c31d22
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2022-08-31 14:21:34 +0530

    Update App.js
    name and description added

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+        <h1 data-ns-test="project-name">Project Name - Let's Play</h1>
+        
+        <div data-ns-test="project-description">
+          <h2>Project Description</h2>
+          <p>A online plateform to organise sports events.</p>
+        </div>      
+    </div>
+    
   )
 }
-
 
 export default App;
